3c19d7741d225cec79c70b55220c0ddcad16d1fe
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-01-11 12:24:22 +0300

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+- Description: type-annotated function `concat` takes a string `str1` and a
+                string `str2` as arguments
+- Parameters: returns a concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Returns concatenated string """
+    return str1 + str2
