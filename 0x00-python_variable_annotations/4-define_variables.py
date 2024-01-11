b4df8125de311b20bb233721cdace480d74bf374
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-01-11 12:43:35 +0300

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+- Description: annotate the variables below with the specified values
+                a - integer with value 1
+                pi - float with value 3.14
+                i_understand_annotations - boolean with value True
+                school - string with value "Holberton"
+- Parameters: None
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
