4205cef4297786a7dc374c24bcdf3746b664d6b7
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-01-17 01:02:27 +0300

    1-concurrent_coroutines.py -- checker fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -29,11 +29,17 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
         list[float]: [description]
     """
     list_delays: List[float] = []
-    for _ in range(n):
-        task = asyncio.create_task(wait_random(max_delay))
-        list_delays.append(task)
+    tasks_delay: List[asyncio.Task] = []
 
-    return [await delay for delay in asyncio.as_completed(list_delays)]
+    for _ in range(n):
+        task = wait_random(max_delay)
+        tasks_delay.append(task)
+
+    for task in asyncio.as_completed(tasks_delay):
+        delay = await task
+        list_delays.append(delay)
+
+    return list_delays
 
 # if __name__ == "__main__":
 #     print(asyncio.run(wait_n(5, 5)))
