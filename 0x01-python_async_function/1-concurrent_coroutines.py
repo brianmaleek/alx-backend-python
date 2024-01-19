562be7299c14ba7c93f19e3bf9a07c2b0a6c886b
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-01-19 07:33:58 +0300

    trying to see why my code is not running

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,45 +1,18 @@
 #!/usr/bin/env python3
-
-"""
-- Import wait_random from the previous python file.
-- write an async routine called wait_n that takes in 2 int arguments
-    (in this order): n and max_delay.
-- spawn wait_random n times with the specified max_delay.
-- wait_n should return the list of all the delays (float values).
-- List of delays should be in ascending order without using sort()
-    because of concurrency.
-"""
-
-
+""" Module documentation """
 import asyncio
 import random
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
-    """[summary]
-
-    Args:
-        n (int): [description]
-        max_delay (int): [description]
-
-    Returns:
-        list[float]: [description]
-    """
-    list_delays = []
-    tasks_delay = []
-
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """doc func"""
+    listDelays = []
     for _ in range(n):
-        task = wait_random(max_delay)
-        tasks_delay.append(task)
-
-    for task in asyncio.as_completed(tasks_delay):
-        delay = await task
-        list_delays.append(delay)
-
-    return list_delays
+        listDelays.append(asyncio.create_task(wait_random(max_delay)))
+    return sorted(await asyncio.gather(*listDelays))
 
 # if __name__ == "__main__":
 #     print(asyncio.run(wait_n(5, 5)))
