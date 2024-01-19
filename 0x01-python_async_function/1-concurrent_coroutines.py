e22704520c35c313702a798a2e251bf24706aef8
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-01-19 07:57:39 +0300

    reverted to original code. code running locally, intranet throwing errors

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
 
-""" module documentation """
+"""
+- Import wait_random from the previous python file.
+- write an async routine called wait_n that takes in 2 int arguments
+    (in this order): n and max_delay.
+- spawn wait_random n times with the specified max_delay.
+- wait_n should return the list of all the delays (float values).
+- List of delays should be in ascending order without using sort()
+    because of concurrency.
+"""
+
+
 import asyncio
-import random
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
-    """ doc function """
+    """
+    Spawn wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): Number of times to spawn wait_random.
+        max_delay (int): Maximum delay value.
+
+    Returns:
+        List[float]: List of delays in ascending order.
+    """
     list_delays = []
     tasks_delay = []
 
@@ -17,8 +35,8 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
         task = wait_random(max_delay)
         tasks_delay.append(task)
 
-    for task in asyncio.as_completed(tasks_delay):
-        delay = await task
+    for completed_task in asyncio.as_completed(tasks_delay):
+        delay = await completed_task
         list_delays.append(delay)
 
     return list_delays
