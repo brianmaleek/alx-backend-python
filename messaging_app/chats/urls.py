a33737bac1272ea7528f22560eb858dc510e2669
Brian Maleek <maleekmalik@gmail.com>
2025-06-08 02:16:54 +0300

    Refactor URL routing in chats app to separate main and authentication endpoints, enhancing organization and clarity.

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, routers
 from .views import ConversationViewSet, MessageViewSet, UserViewSet
 
 router = DefaultRouter()
@@ -7,6 +7,16 @@ router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 router.register(r'users', UserViewSet, basename='user')
 
-urlpatterns = [
+# Create regular router for user endpoints
+auth_router = routers.DefaultRouter()
+auth_router.register(r'users', UserViewSet, basename='user')
+
+# main urlpatterns
+main_urlpatterns = [
     path('', include(router.urls)),
+]
+
+# auth urlpatterns
+auth_urlpatterns = [
+    path('', include(auth_router.urls)),
 ]
