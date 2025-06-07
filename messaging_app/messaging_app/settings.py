05f631b7d1f9f882756328b89d4847549a48018b
Brian Maleek <maleekmalik@gmail.com>
2025-06-08 02:17:08 +0300

    Add Django Filters and Pagination to REST Framework Settings for Enhanced Querying

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'drf_yasg',
     'chats',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -133,6 +134,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 # Custom User Model
